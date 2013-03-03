3996522b749ca7841bfc03b1c558c3198f56c44e
michalbali <michalbali@a6db24f8-6719-11de-8ec2-fbb090c18dad>
2013-03-03 08:40:07 +0000

    chapter 11 - adding camelServer web app, updating client

--- trunk/droolsBookParent/integration/client/interest_request.rb
+++ trunk/droolsBookParent/integration/client/interest_request.rb
@@ -1,19 +1,30 @@
 require 'net/http'
 require 'json'
 http = Net::HTTP.new('localhost', 8080)
-path = "/drools-server/knowledgebase/interestcalculation"
+path = "/camelServer/kservice/rest/execute"
 headers = {
-  "Content-Type" => "application/json"
+  "Content-Type" => "text/plain"
 }
-post_data = {"knowledgebase-request" => {                     
-  :inOutFacts => {
-    "named-fact" => [ { :id => "account", :fact => {
-      "@class"=>"droolsbook.decisiontables.bank.model.Account",
-      "type" => "STUDENT", "balance" => "1000", 
-      "currency" => "EUR" } } ] 
-  }
+post_data = {"batch-execution" => {                     
+  "commands" => [ 
+    { 
+      "insert" => {
+        "out-identifier" => "account",
+        "return-object" => true,
+        "object" => { 
+          "droolsbook.decisiontables.bank.model.Account" => {
+            "type" => "STUDENT",
+            "balance" => "1000", 
+            "currency" => "EUR" 
+          }  
+        }
+      }
+    },
+    {
+      "fire-all-rules" => ""
+    }
+  ]  
 }}
 resp, data = http.post(path, post_data.to_json, headers)
 answer = JSON.parse(data)
-puts answer["knowledgebase-response"]["inOutFacts"]\
-["named-fact"][0]["fact"]["interestRate"]
+puts answer["execution-results"]["results"]["result"]["value"]["droolsbook.decisiontables.bank.model.Account"]["interestRate"]
